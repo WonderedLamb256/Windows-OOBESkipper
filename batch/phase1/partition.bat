4d6caa39ea3ec7d56c9b5211b24d5093bd441965
WonderedLamb256 <41864659+WonderedLamb256@users.noreply.github.com>
2020-02-23 11:30:32 -0500

    Create partition.bat

---
+++ batch/phase1/partition.bat
@@ -0,0 +1,15 @@
+set /p isUsingEFI="Are you using EFI? Type in Y or y for yes > "
+if not defined isUsingEFI (
+      echo ERROR: The variable isUsingEFI is undefined. Stop.
+      exit 1
+)
+echo With great power comes great responsibility. Press Ctrl+C to exit or any key to continue to partitioning.
+pause
+echo OOBE Skipper is using diskpart to partition the disk...
+if /i isUsingEFI == "Y" (
+      diskpart D:\OOBESkipper\config\diskpart.txt
+) else (
+      diskpart D:\OOBESkipper\config\diskpart-legacy.txt
+      rem The diskpart script diskpart-legacy.txt is a BIOS version of diskpart.txt.
+)
+echo OOBE Skipper is handing the process chain to Stage 1B - image.bat (Imaging)
