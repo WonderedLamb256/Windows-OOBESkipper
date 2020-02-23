9ecff3c1d6f2540c49c8c0e6f937eb774dee9959
WonderedLamb256 <41864659+WonderedLamb256@users.noreply.github.com>
2020-02-23 11:01:15 -0500

    Create install.bat

---
+++ install.bat
@@ -0,0 +1,13 @@
+@echo off
+echo Welcome! The Windows OOBE Skipper is an open-source script that will reformat your computer and install Windows 10 on it.
+pause
+if exist "batch-check-file.txt" (
+      goto nocopy
+) else (
+      echo OOBE Skipper is copying files to the base of the D drive...
+      copy * D:/
+)
+:nocopy
+echo OOBE Skipper is beginning Phase 1A - system partitioning...
+rem Hand control over to partition.bat
+D:/phase1/partition.bat
